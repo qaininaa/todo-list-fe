0445937480c7c8fc02b292eb6f41763424990f07
Karina Ghaisani <karinaghsn@gmail.com>
2025-08-15 16:14:04 +0700

    Refactor: "Context API for access token"

---
+++ src/context/auth/AuthContext.tsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+interface AuthContextType {
+  user: string | null;
+  login: (username: string) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
