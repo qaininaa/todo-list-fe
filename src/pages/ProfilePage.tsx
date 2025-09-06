b8ba4eb61b31c7e49b59aa8a65f1b87185b214fa
Karina Ghaisani <karinaghsn@gmail.com>
2025-09-06 09:56:18 +0700

    Refactor: profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,44 +1,8 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import useAxiosInstance from "../hooks/useAxiosInstance";
-import useAuth from "../hooks/useAuth";
-import useRefreshAccess from "../hooks/useRefreshAccess";
-import axiosInstance from "../api/axios";
-import axios from "axios";
-
-interface User {
-  name: string;
-  username: string;
-  email: string;
-  iat: number;
-  exp: number;
-}
 
 const ProfilePage = () => {
-  const [user, setUser] = useState<null | User>(null);
   const navigate = useNavigate();
-  const axios = useAxiosInstance();
-  const auth = useAuth();
-  const [mounted, setMounted] = useState(true);
-  const refresh = useRefreshAccess();
 
-  useEffect(() => {
-    console.log("ProfilePage mounted ✅");
-
-    const checkRefresh = async () => {
-      try {
-        const res = await axios.get(`http://localhost:3000/api/auth/refresh`, {
-          withCredentials: true,
-        });
-
-        console.info("New Access Token:", res.data.accessToken);
-      } catch (err) {
-        console.error("Refresh failed:", err);
-      }
-    };
-
-    checkRefresh();
-  }, []);
   return (
     <div>
       <button onClick={() => navigate("/")}>home</button>
