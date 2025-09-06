00f3c99b6d6193817c107ba1d9f8ec00717461ad
Karina Ghaisani <karinaghsn@gmail.com>
2025-09-06 11:17:33 +0700

    Refactor: fetch data user

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useAxiosInstance from "../hooks/useAxiosInstance";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
+  const [profile, setProfile] = useState();
+  const axios = useAxiosInstance();
+
+  useEffect(() => {
+    let mounted: boolean = true;
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/profile/user", {
+          signal: controller.signal,
+        });
+        console.log("ini response profile", response);
+
+        mounted && setProfile(response.data.user);
+      } catch (error) {}
+    };
+    fetchData();
+
+    return () => {
+      (mounted = false), controller.abort();
+    };
+  }, [axios]);
 
   return (
     <div>
       <button onClick={() => navigate("/")}>home</button>
+      {profile && <p>{profile?.email}</p>}
     </div>
   );
 };
